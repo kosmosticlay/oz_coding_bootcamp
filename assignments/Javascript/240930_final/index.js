d9d009f7f4cee2c5327c1bb6ad237d919bb3f7eb
kosmosticlay <kosmosticlay@gmail.com>
2024-09-26 20:08:40 +0900

    feat: Add basic calculator functionalities (display, number input, decimal, clear)

--- assignments/Javascript/240930_final/index.js
+++ assignments/Javascript/240930_final/index.js
@@ -1,1 +1,35 @@
 const $buttons = document.querySelectorAll(".button");
+const $display = document.querySelector(".display");
+const $result = $display.querySelector(".result");
+
+let displayValue = 0;
+
+$buttons.forEach(($button) => {
+  $button.addEventListener("click", () => {
+    // console에 각 버튼의 value 출력하기
+    console.log($button.textContent);
+
+    // (화면 기준) 글자 23개 이상이면 이후 입력 무시하기
+    if ($result.textContent.length >= 20) return;
+
+    // 숫자를 디스플레이에 표시하기
+    if ($button.className.includes("number")) {
+      if ($result.textContent === "0") {
+        $result.textContent = $button.textContent;
+      } else {
+        $result.textContent += $button.textContent;
+      }
+    }
+    // 소수점 버튼 추가하기
+    if ($button.textContent === ".") {
+      if (!$result.textContent.includes(".")) {
+        $result.textContent += ".";
+      }
+    }
+
+    // Clear 기능 추가하기
+    if ($button.textContent === "C") {
+      $result.textContent = "0";
+    }
+  });
+});
