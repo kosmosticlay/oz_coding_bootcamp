2c6f6220305f8466698223a5f878b3360aa9cc15
kosmosticlay <kosmosticlay@gmail.com>
2024-11-04 18:14:55 +0900

    feat: Searchbar에 디바운스 기능 추가
    - useDebounce 커스텀 훅으로 입력이 멈춘 후에만 최신 값으로 업데이트

--- assignments/React/PokeWiki/src/components/commons/SearchBar.jsx
+++ assignments/React/PokeWiki/src/components/commons/SearchBar.jsx
@@ -1,11 +1,18 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { SearchContext } from "../../pages/Search";
+import useDebounce from "../../hooks/useDebounce";
 
 export default function SearchBar() {
   const { keyword, setKeyword } = useContext(SearchContext);
+  const [inputValue, setInputValue] = useState(keyword);
+  const debouncedValue = useDebounce(inputValue, 300);
+
+  useEffect(() => {
+    setKeyword(debouncedValue);
+  }, [debouncedValue, setKeyword]);
 
   const handleInputChange = (event) => {
-    setKeyword(event.target.value);
+    setInputValue(event.target.value);
   };
 
   return (
@@ -14,7 +21,7 @@ export default function SearchBar() {
         className="font-korean min-w-[200px] w-1/4 h-10 pb-2 border-b-2 border-[#FEC20C] bg-transparent outline-none focus:border-red-700 text-center"
         type="text"
         placeholder="포켓몬 이름 입력"
-        value={keyword}
+        value={inputValue}
         onChange={handleInputChange}
       />
     </div>
